b4f0c40899346b12e48f0ef3caff20d9060464da
kham1508 <107246450+kham1508@users.noreply.github.com>
2022-07-26 22:16:17 +0100

    revert header

--- src/lib/layout/NISRAHeader.svelte
+++ src/lib/layout/NISRAHeader.svelte
@@ -1,46 +1,47 @@
 <script>
-  import { assets } from "$app/paths";
-	import { getContext } from 'svelte';
-
-	export let theme = getContext('theme');
-	export let filled = true;
-	export let center = false;
-</script>
-
-<nav style="background-color: #ffffff">
-  <div class="col-wide middle" class:center>
-		<a href="https://www.nisra.gov.uk/">
-			<picture>
-				{#if theme.name == 'dark'}
-				<img src="{assets}/img/nisra-full-name-stacked-white.svg" alt="Northern Ireland Statistics and Research Agency">
-				{:else if filled == true}
-				<img src="{assets}/img/nisra-full-name-stacked-colour.svg" alt="Northern Ireland Statistics and Research Agency">
-				{:else}
-				<img src="{assets}/img/nisra-full-name-stacked-colour.svg" alt="Northern Ireland Statistics and Research Agency">
-				{/if}
-			</picture>
-		</a>
-  </div>
-</nav>
-
-<style>
-	nav {
-		-webkit-box-sizing: border-box;
-		box-sizing: border-box;
-		position: relative;
-		height: 45px;
-		border-bottom: 1px solid #777;
-		z-index: 1;
-	}
-	picture {
-		position: relative;
-		top: 1px;
-		padding: 0 2px;
-	}
-	img {
-		width: 120px;
-	}
-	a img:hover {
-		cursor: pointer;
-	}
-</style>
+	import { assets } from "$app/paths";
+	  import { getContext } from 'svelte';
+  
+	  export let theme = getContext('theme');
+	  export let filled = true;
+	  export let center = false;
+  </script>
+  
+  <nav style="border-bottom-color: {theme['muted']}; {filled ? 'background-color: ' +  theme['background'] + ';' : ''}">
+	<div class="col-wide middle" class:center>
+		  <a href="https://www.nisra.gov.uk/">
+			  <picture>
+				  {#if theme.name == 'dark'}
+				  <img src="{assets}/img/nisra-full-name-stacked-white.svg" alt="Northern Ireland Statistics and Research Agency">
+				  {:else if filled == true}
+				  <img src="{assets}/img/nisra-full-name-stacked-colour.svg" alt="Northern Ireland Statistics and Research Agency">
+				  {:else}
+				  <img src="{assets}/img/nisra-full-name-stacked-colour.svg" alt="Northern Ireland Statistics and Research Agency">
+				  {/if}
+			  </picture>
+		  </a>
+	</div>
+  </nav>
+  
+  <style>
+	  nav {
+		  -webkit-box-sizing: border-box;
+		  box-sizing: border-box;
+		  position: relative;
+		  height: 46px;
+		  margin-bottom: -46px;
+		  border-bottom: 1px solid #777;
+		  z-index: 1;
+	  }
+	  picture {
+		  position: relative;
+		  top: -3px;
+		  padding: 0 2px;
+	  }
+	  img {
+		  width: 120px;
+	  }
+	  a img:hover {
+		  cursor: pointer;
+	  }
+  </style>
